b22ab5434bd31463b19984731b8fb0f8f4a4e726
Huynguyenboiz <huynguyen2366@gmail.com>
2026-01-17 09:48:55 +0700

    opt

--- components/PhotoCarousel.tsx
+++ components/PhotoCarousel.tsx
@@ -6,7 +6,7 @@ interface Props {
   onSelect: (memory: FriendMemory) => void;
 }
 
-const AUTOPLAY_MS = 3000;
+const AUTOPLAY_MS = 2000;
 const SWIPE_THRESHOLD = 50;
 
 const mod = (n: number, m: number) => ((n % m) + m) % m;
